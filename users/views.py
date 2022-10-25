b59ebad56beaac2690279cf87cf605b95f48f437
Zamarin-Alexey <91373169+Zamarin-Alexey@users.noreply.github.com>
2022-10-25 17:30:11 +0300

    Добавлена страница блога

--- users/views.py
+++ users/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 
 def login(request):
-    return render(request, 'users/login_form.html', {'title': 'Авторизация', 'description': 'Форма авторизации'})
+    return render(request, 'users/login.html', {'title': 'Авторизация', 'description': 'Форма авторизации'})
+
+
+def registration(request):
+    form = UserCreationForm()
+    return render(request, 'users/registration.html',
+                  {'title': 'Регистрация', 'description': 'Форма регистрации', 'form': form})
