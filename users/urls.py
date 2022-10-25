f6678cbbd6c9d7969f1bdae516ec2b2a0620569f
Zamarin-Alexey <91373169+Zamarin-Alexey@users.noreply.github.com>
2022-10-25 17:24:35 +0300

    Добавлена форма регистрации (пока не рабочая)

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('login/', login),
+    path('login/', login, name='login'),
+    path('reg/', registration, name='registration')
 ]
